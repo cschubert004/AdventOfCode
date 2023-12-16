45cce0b77f86579604f1c19b639d70427b64b883
cschubert004 <cschubert004@users.noreply.github.com>
2023-12-15 20:16:38 -0800

    day 13 part 2

--- day13/day13.py
+++ day13/day13.py
@@ -9,7 +9,7 @@ def transpose_text_block(input_text:str)->str:
     return transposed_text
 
 
-def get_reflection(puzzle_text:str, print_debug = False)->int:
+def get_reflection(puzzle_text:str, diff_letter_count =0)->int:
     # look for horizontal reflections.
     # return the 0-indexed row number where the refelction starts.
     # return 0 if there is no reflection
@@ -24,33 +24,36 @@ def get_reflection(puzzle_text:str, print_debug = False)->int:
         rows_to_concat = min(row,puzzle_rows-row)
         above_text = "".join(puzzle_text[row-rows_to_concat:row][::-1])
         below_text = "".join(puzzle_text[row:row+rows_to_concat])
-        if above_text == below_text:
+        if count_diff_letters(above_text,below_text) == diff_letter_count:
             reflection_row = row
 
     return reflection_row
 
 
+def count_diff_letters(str_a:str,str_b:str)->int:
+    # get the number of different characters
+    return sum ( str_a[i] != str_b[i] for i in range(len(str_a)) )
 
-def do_part_one(puzzle_list:list[str])->int:
+
+def do_part_two(puzzle_list:list[str], diff_letter_count)->int:
     puzzle_sum = 0
     for puzzle in puzzle_list:
         # look for a horizontal reflection
-        puzzle_reflection_row = get_reflection(puzzle.splitlines())
+        puzzle_reflection_row = get_reflection(puzzle.splitlines(),diff_letter_count)
         # transpose and look for a vertical refelction
         transposed_text = transpose_text_block(puzzle)
-        puzzle_reflection_col = get_reflection(transposed_text)
-        print (f"Reflections row:{puzzle_reflection_row}, col:{puzzle_reflection_col}")
+        puzzle_reflection_col = get_reflection(transposed_text,diff_letter_count)
+        # debug print
+        #print (f"Reflections row:{puzzle_reflection_row}, col:{puzzle_reflection_col}")
         puzzle_sum+= (puzzle_reflection_col+(100*puzzle_reflection_row))
     
-    print (f"Puzzle sume = {puzzle_sum}")
+    print (f"Puzzle sum = {puzzle_sum}")
 
-def do_part_two():
-    pass
 
 
 if __name__ == "__main__":
     #puzzles = (input_data.day13_test_input).split("\n\n")
     puzzles = (input_data.day13_input).split("\n\n")
 
-    do_part_one(puzzles)
-    do_part_two()  # in progress
+    do_part_two(puzzles,0)
+    do_part_two(puzzles,1)  # in progress
