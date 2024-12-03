11ee54b0cc5dcb21bab9783790fe5ad64af40299
cschubert004 <cschubert004@users.noreply.github.com>
2024-12-02 21:59:11 -0800

    day 2 completed

--- 2024/day-02/day2.py
+++ 2024/day-02/day2.py
@@ -14,76 +14,62 @@ def parse_data(example=False):
     return data
 
 
+def check_levels(level):
+    direction_map = {0:[0,-1,1], -1:[-1], 1:[1]}
+    direction = 0
+    safe_threshold =3    
+    idx = 0
+    safe = True
+    while idx < (len(level)-1):
+        delta = level[idx+1] - level[idx]
+        if delta != 0:
+            # normalize value, then check if it is in the allowed direction
+            delta_n = delta/abs(delta)
+            if delta_n in direction_map[direction]:
+                direction = delta_n
+            else:
+                return False
+        if abs(delta) > safe_threshold or abs(delta) < 1:
+            return False
 
-direction_map = {0:[0,-1,1], -1:[-1], 1:[1]}
+        idx+=1
+
+    return True
+
 
 def do_part_one(data):
-    safe_threshold =3
-    num_safe_levels=0
+    num_safe_levels = 0
     for level in data:
-        safe = True
-        direction = 0
-        idx = 0
-        while idx < (len(level)-1) and safe:
-            delta = level[idx+1] - level[idx]
-            if delta != 0:
-                # normalize value, then check if it is in the allowed direction
-                delta_n = delta/abs(delta)
-                if delta_n in direction_map[direction]:
-                    direction = delta_n
-                else:
-                    safe = False
-            if abs(delta) > safe_threshold or abs(delta) < 1:
-                safe = False
-            idx+=1
-        
-        if safe:
+        if check_levels(level):
             num_safe_levels += 1
 
     print (f"Part 1:\n\tNumber of safe levels = {num_safe_levels}")
-
+    return num_safe_levels
 
 
 def do_part_two(data):
-    safe_threshold =3
-    num_safe_levels=0
-    retry_levels = []
+    num_safe_levels = 0
     for level in data:
-        safe = True
-        direction = 0
-        idx = 0
-        while idx < (len(level)-1) and safe:
-            retry_level = None
-            delta = level[idx+1] - level[idx]
-            if delta != 0:
-                # normalize value, then check if it is in the allowed direction
-                delta_n = delta/abs(delta)
-                if delta_n in direction_map[direction]:
-                    direction = delta_n
-                else:
-                    safe = False
-                    # remove the next level and rerty it
-                    retry_level = level
-                    retry_level.pop(idx+1)
-
-            if safe and (abs(delta) > safe_threshold or abs(delta) < 1):
-                safe = False
-                retry_level = level
-                retry_level.pop(idx+1)
-
-            idx+=1
-        
-        if safe:
+        if check_levels(level):
             num_safe_levels += 1
-        if retry_level is not None:
-            retry_levels.append(retry_level)
-
+        else:
+            # check if one of the level subsets is valid
+            subsets = []
+            for idx in range (len(level)):
+                subset = level.copy()
+                subset.pop(idx)
+                subsets.append(subset)
+            # check each subset - iof any are valid, the level is safe.
+            for subset in subsets:
+                if check_levels(subset):
+                    num_safe_levels += 1
+                    break
     print (f"Part 2:\n\tNumber of safe levels = {num_safe_levels}")
-    return retry_levels
+    return num_safe_levels
+
 
 if __name__ == "__main__":
     data = parse_data(False)
 
-    # do_part_one(data)
-    retries = do_part_two(data)  # in progress
-    do_part_two(retries)
+    do_part_one(data)
+    do_part_two(data)
